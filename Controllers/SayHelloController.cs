1fe3cd2da2f63e66d578112180ac963872313980
Chris Laine <christopher.ian.laine@gmail.com>
2021-07-28 17:07:08 +1200

    Adding the base code for this article, including a simple dotnet api and docker-compose logic for traefik

---
+++ Controllers/SayHelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TraefikExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SayHelloController : ControllerBase
+    {
+
+        private readonly ILogger<SayHelloController> _logger;
+
+        public SayHelloController(ILogger<SayHelloController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Howdy from HTTPS via Traefik";
+        }
+    }
+}
---
+++ certs/README.md
@@ -0,0 +1,4 @@
+# Certs go in here
+You can place your cert public and private key in this directory.
+
+See traefik/config.yml for more info on file naming.
